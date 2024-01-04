0a8bc644d1bd034ef41628e1773eb6559c0767cb
Hailun <140507861+LeHai-HaiLun@users.noreply.github.com>
2024-01-04 08:41:07 +0700

    Create cb07.cpp

---
+++ cb07.cpp
@@ -0,0 +1,10 @@
+/*Hailun*/
+#include<iostream>
+using namespace std;
+int main(int agrc, char* argv[])
+{
+    float r;
+    cin >> r;
+    cout << 2 * r * 3.14 << " " << r * r * 3.14;
+    return 0;
+}
