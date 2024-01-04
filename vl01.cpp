4e57ce7168b65caf34b85b3ec205638600fe5fe1
Hailun <140507861+LeHai-HaiLun@users.noreply.github.com>
2024-01-04 14:49:09 +0700

    Create vl01.cpp

---
+++ vl01.cpp
@@ -0,0 +1,15 @@
+/*Hailun*/
+#include "iostream"
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    // for(int i = a; i<=b; i++)   cout << i << " ";
+    while(a <= b)
+    {
+        cout << a << " ";
+        a++;
+    }
+    return 0;
+}
