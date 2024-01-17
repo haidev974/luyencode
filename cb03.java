3ad467d86c20d5bdcd5447a40be172b6df9e9719
Hailun <140507861+LeHai-HaiLun@users.noreply.github.com>
2024-01-17 16:13:17 +0700

    Create cb03.java

---
+++ cb03.java
@@ -0,0 +1,14 @@
+package luyencode;
+
+import java.util.Scanner;
+
+public class cb03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(a + b + c);
+        sc.close();
+    }
+}
