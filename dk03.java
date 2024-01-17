73621997443e03b6e86d3ea34afd7f33f52f1891
Hailun <140507861+LeHai-HaiLun@users.noreply.github.com>
2024-01-17 16:28:25 +0700

    Create dk03.java

---
+++ dk03.java
@@ -0,0 +1,13 @@
+package luyencode;
+
+import java.util.Scanner;
+
+public class dk03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(Math.abs(a - b));
+        sc.close();
+    }
+}
